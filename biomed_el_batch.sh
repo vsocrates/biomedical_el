85b2128bd76f4b5093e55f9b1fc88fe9d41dd82b
Vimig Socrates <vimig.socrates@gmail.com>
2021-06-16 23:55:06 -0400

    grid search output

--- biomed_el_batch.sh
+++ biomed_el_batch.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-
-for i in 4 
+for i in 4 8 16
 do
-	for j in 0.00001 0.00002
+	for j in  0.00002 0.0001
 	do
-		sbatch --job-name=bioel_5.$i.$j --output=bioel_5.$i.$j.out biomed_el_job.slurm 'pretraining5' $i $j 5
+		for stringval in pretraining pretraining2 pretraining3 pretraining4
+		do
+			sbatch --job-name=bioel_$stringval.$i.$j --output=bioel_$stringval.$i.$j.out biomed_el_job.slurm $stringval  $i $j 25
+		done
 	done
 done
 
